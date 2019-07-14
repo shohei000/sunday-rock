dbdbe8324deb504ec4c9fe0abe9f5a7f1cc58dab
saino <sainoshohei@dande-lion.co.jp>
2019-07-14 21:57:01 +0900

    リスト回収

--- database/seeds/MessagesTableSeeder.php
+++ database/seeds/MessagesTableSeeder.php
@@ -15,16 +15,16 @@ class MessagesTableSeeder extends Seeder
       //User テーブル全けし
       Message::truncate();
 
-      $faker = Faker\Factory::create('ja_JP');
-      foreach (range(0, 400) as $num) {
-        DB::table('messages')->insert([
-          'user_id' => $num,
-          'thread_id' => rand(1,100),
-          'text' => $faker->realText($maxNbChars = rand(10,50), $indexSize = 2),
-          'created_at' => date('Y/m/d H:i:s'),
-          'updated_at' => date('Y/m/d H:i:s'),
-        ]);
-      }
+      // $faker = Faker\Factory::create('ja_JP');
+      // foreach (range(0, 400) as $num) {
+      //   DB::table('messages')->insert([
+      //     'user_id' => $num,
+      //     'thread_id' => rand(1,100),
+      //     'text' => $faker->realText($maxNbChars = rand(10,50), $indexSize = 2),
+      //     'created_at' => date('Y/m/d H:i:s'),
+      //     'updated_at' => date('Y/m/d H:i:s'),
+      //   ]);
+      // }
 
     }
 }
